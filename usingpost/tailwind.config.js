5cff8467bbc5ff48d628c12305845349e05e3ecb
Hsu Myat Moe <hsumyatm7308@gmail.com>
2022-09-17 00:07:21 +0630

    12:07AM

---
+++ usingpost/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+// ** is all folder of inside in public folder 
+// * is all html file 
