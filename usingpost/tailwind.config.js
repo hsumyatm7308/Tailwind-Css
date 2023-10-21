eb0df6fb3fb723f88493dd2b50de2ce50cd25f5a
JV <107622230+hsumyatm7308@users.noreply.github.com>
2023-10-21 21:40:08 +0630

    Add files via upload

--- usingpost/tailwind.config.js
+++ usingpost/tailwind.config.js
@@ -3,10 +3,7 @@ module.exports = {
   content: ["./public/**/*.html"],
   theme: {
     extend: {},
+   
   },
   plugins: [],
 }
-
-
-// ** is all folder of inside in public folder 
-// * is all html file 
