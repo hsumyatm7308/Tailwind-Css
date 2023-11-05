ee6bc51cac119b4852bc194d67b24ef57a0dad3f
JV <107622230+hsumyatm7308@users.noreply.github.com>
2023-11-05 21:15:49 +0630

    Add files via upload

--- usingpost/tailwind.config.js
+++ usingpost/tailwind.config.js
@@ -2,8 +2,44 @@
 module.exports = {
   content: ["./public/**/*.html"],
   theme: {
-    extend: {},
-   
+    extend: {
+      animation:{
+        'spin-slow': 'spin 3s linear infinite',
+        'myani': 'mykeyone 5s linear infinite alternate',
+        'myrotate': 'mykeytwo 5s linear infinite',
+        'yourani': 'mykeythree 5s linear infinite'
+      },
+      keyframes:{
+        mykeyone:{
+          '0%':{transfrom:'translate(0)'},
+          '50%':{transfrom:'translate(300px)'},
+          '100%':{transfrom:'translate(300px,300px)'}
+        },
+        mykeytwo:{
+          'from':{transfrom:'rotate(0deg)'},
+          'to':{transfrom:'rotate(360deg)'}
+        },
+        mykeythree:{
+          '0%,100%':{transfrom:'rotate(0deg)'},
+          '50%':{transfrom:'rotate(-1turn)'}
+        }
+      },
+
+      colors:{
+        primary:"blue",
+        secondary:"silver",
+        info:"skyblue",
+        success:"lime",
+        danger:"red"
+      },
+
+      fontFamily:{
+        aria:["Arial",'sans-serif'],
+        times:"Times New Roman",
+        mono:"monospace"
+      }
+
+    },
   },
   plugins: [],
 }
