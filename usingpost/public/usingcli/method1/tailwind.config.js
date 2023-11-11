7846accaff56d0529090d3285473817ce6d4dd53
JV <107622230+hsumyatm7308@users.noreply.github.com>
2023-11-11 21:49:00 +0630

    Add files via upload

--- usingpost/public/usingcli/method1/tailwind.config.js
+++ usingpost/public/usingcli/method1/tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    theme:{
+      sm:'640px',
+      md:'768px',
+      lg:'1024px',
+      xl:'1280px'
+    },
+
+    
+     extend: {
+      colors: {
+        
+      }
+     },
+   
   },
   plugins: [],
 }
